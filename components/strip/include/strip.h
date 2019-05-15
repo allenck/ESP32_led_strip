204a2e21947a032403531432d234500681ebe81c
Allen Kempe <allenck@windstream.net>
2019-05-15 00:10:29 -0400

    create initial RGBWstrandtestCpp files

---
+++ components/strip/include/strip.h
@@ -0,0 +1,25 @@
+/*  ---------------------------------------------------------------------------
+    File: strip.h
+    Author(s):  Allen Kempe <allenck@windstream.net>
+    Date Created: 05/14/2019
+    Last modified: 05/14/2019
+    Description: 
+    This library can drive led strips through the RMT module on the ESP32.
+    ------------------------------------------------------------------------ */
+
+#ifndef STRIP_H
+#define STRIP_H
+
+extern "C"
+{
+ #include "led_strip.h"
+}
+class Strip
+{
+ public:
+ Strip();
+
+ private:
+
+};
+#endif
---
+++ components/strip/strip.cpp
@@ -0,0 +1,14 @@
+/*  ---------------------------------------------------------------------------
+    File: strip.cpp
+    Author(s):  Allen Kempe <allenck@windstream.net>
+    Date Created: 05/14/2019
+    Last modified: 05/14/2019
+    Description: 
+    This library can drive led strips through the RMT module on the ESP32.
+    ------------------------------------------------------------------------ */
+#include "strip.h"
+
+Strip::Strip()
+{
+}
+
